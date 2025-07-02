a7d250ff70c9a9aedc334fb57ebaf733f43cb7f4
Joulos-dev <jules.fournol@hotmail.com>
2025-07-02 13:23:32 +0200

    dzqd
    qzdzq

--- docker/index.php
+++ docker/index.php
@@ -1,3 +1,16 @@
-<?php
+<?php var_dump("coucou je fonctionne"); ?>
 
-var_dump("coucou je fonctionne");
+<div>
+    <?php echo 'My username is ' .$_ENV["DB_USER"] . " and my password is " . $_ENV["DB_PASSWORD"]; ?>
+
+</div>
+
+<div>
+    <?php echo 'My name is ' .$_ENV["DB_NAME"] . " and " . $_ENV["DB_COUCOU"]; ?>
+
+</div>
+
+
+
+
+
